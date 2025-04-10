528c284f9d63c202d87379541670cec3036972bb
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-04-10 14:04:01 +0300

    Add edit options

--- Lab2/Console-based document editor/Console-based document editor/Document.cpp
+++ Lab2/Console-based document editor/Console-based document editor/Document.cpp
@@ -1,12 +1,9 @@
 ﻿#include "Document.h"
 
 
-static bool aa = true;
-
-
 
 void Document::Edit() {
-	aa = true;
+	UpdateInformation();
 
 	//std::cout << "Aditing is allowed for: " << "\n";
 	//std::cout << "Manage permissions." << "\n";
@@ -20,13 +17,13 @@ void Document::Edit() {
 		bool is_ctrl = false;
 		is_ctrl = GetAsyncKeyState(17) < 0;
 		bool is_alt = false;
-		is_ctrl = GetAsyncKeyState(18) < 0;
+		is_alt = GetAsyncKeyState(18) < 0;
 
 		for (int key = 0; key < 256; key++) {
 			if (GetAsyncKeyState(key) & 0x8000){
 				if (key == 37) {
 					if (is_shift) {
-
+						HighlightLeft();
 					}
 					else {
 						MoveCursorLeft();
@@ -34,7 +31,7 @@ void Document::Edit() {
 				}
 				if (key == 39) {
 					if (is_shift) {
-
+						HighlightRight();
 					}
 					else {
 						MoveCursorRight();
@@ -70,49 +67,98 @@ void Document::Edit() {
 
 				if (key == 8) {
 					std::cout << '\b' << ' ' << '\b';
+				}*/
+
+				if (key == 8) {
+					DelChar();
 				}
 
-				if (key == 9) std::cout << "  ";
+				if (key == 13) {
+					NewLine();
+				}
 
-				if (key == 32) std::cout << ' ';
+				if (key == 38) {
+					MoveCursorUp();
+				}
+				if (key == 40) {
+					MoveCursorDown();
+				}
 
-				if (key == 192) std::cout << (is_shift ? '~' : '`');
+				if (key == 9) {
+					AddChar(' ');
+					AddChar(' ');
+					AddChar(' ');
+				}			
 
-				if (key == 49) std::cout << (is_shift ? '!' : '1');
-				if (key == 50) std::cout << (is_shift ? '@' : '2');
-				if (key == 51) std::cout << (is_shift ? '#' : '3');
-				if (key == 52) std::cout << (is_shift ? '$' : '4');
-				if (key == 53) std::cout << (is_shift ? '%' : '5');
-				if (key == 54) std::cout << (is_shift ? '^' : '6');
-				if (key == 55) std::cout << (is_shift ? '&' : '7');
-				if (key == 56) std::cout << (is_shift ? '*' : '8');
-				if (key == 57) std::cout << (is_shift ? '(' : '9');
+				if (key == 32) AddChar(' ');
 
-				if (key == 48) std::cout << (is_shift ? ')' : '0');
+				if (key == 192) AddChar((is_shift ? '~' : '`'));
 
-				if (key == 189) std::cout << (is_shift ? '_' : '-');
+				if (key == 49) AddChar((is_shift ? '!' : '1'));
+				if (key == 50) AddChar((is_shift ? '@' : '2'));
+				if (key == 51) AddChar((is_shift ? '#' : '3'));
+				if (key == 52) AddChar((is_shift ? '$' : '4'));
+				if (key == 53) AddChar((is_shift ? '%' : '5'));
+				if (key == 54) AddChar((is_shift ? '^' : '6'));
+				if (key == 55) AddChar((is_shift ? '&' : '7'));
+				if (key == 56) AddChar((is_shift ? '*' : '8'));
+				if (key == 57) AddChar((is_shift ? '(' : '9'));
 
-				if (key == 187) std::cout << (is_shift ? '+' : '=');
-				if (key == 191) std::cout << (is_shift ? '?' : '/');
-				if (key == 190) std::cout << (is_shift ? '>' : '.');
-				if (key == 188) std::cout << (is_shift ? '<' : ',');
+				if (key == 48) AddChar((is_shift ? ')' : '0'));
 
-				if (key == 220) std::cout << (is_shift ? '|' : '\\');
-				if (key == 219) std::cout << (is_shift ? '{' : '[');
-				if (key == 221) std::cout << (is_shift ? '}' : ']');
-				if (key == 186) std::cout << (is_shift ? ':' : ';');
-				if (key == 222) std::cout << (is_shift ? '"' : '\'');*/
+				if (key == 189) AddChar((is_shift ? '_' : '-'));
+
+				if (key == 187) AddChar((is_shift ? '+' : '='));
+				if (key == 191) AddChar((is_shift ? '?' : '/'));
+				if (key == 190) AddChar((is_shift ? '>' : '.'));
+				if (key == 188) AddChar((is_shift ? '<' : ','));
+
+				if (key == 220) AddChar((is_shift ? '|' : '\\'));
+				if (key == 219) AddChar((is_shift ? '{' : '['));
+				if (key == 221) AddChar((is_shift ? '}' : ']'));
+				if (key == 186) AddChar((is_shift ? ':' : ';'));
+				if (key == 222) AddChar((is_shift ? '"' : '\''));
 
 
 				if (key >= 'A' && key <= 'Z') {
-					if (!is_shift) {
+					if (is_ctrl) {
+						if (key == 'I') {
+							SwitchInsert();
+						}
+						if (key == 'P') {
+							CopyToClipboard();
+						}
+						if (key == 'V') {
+							HANDLE clip = 0;
 
-						//std::cout << "\033[K";
-						AddChar(false, (char)(key + 'a' - 'A'));
+							if (OpenClipboard(NULL)) {
+								clip = GetClipboardData(CF_TEXT);
+								CloseClipboard();
+							}
+
+
+							//string text;
+							//text = (char*)clip;
+							char* a = (char*)clip;
+								if (a) {
+									while (*a) {
+										AddChar(*a);
+										a++;
+									}
+								}
+								
+						}
 					}
 					else {
-						AddChar(false, (char)key);
+						
+						if (!is_shift) {
+							AddChar((char)(key + 'a' - 'A'));
+						}
+						else {
+							AddChar((char)key);
+						}
 					}
+					
 				}
 			}
 		}
--- Lab2/Console-based document editor/Console-based document editor/Document.h
+++ Lab2/Console-based document editor/Console-based document editor/Document.h
@@ -5,6 +5,8 @@
 #include <thread>
 #include <ctime>
 
+#include "Auth/User.h"
+
 
 
 extern bool G_is_editor_rinning;
@@ -14,44 +16,91 @@ class Line {
 public:
 	Line(int line_number) {
 		m_line_number = line_number;
+
+		ClearHighlight();
 	}
 
 	void Print() {
 		std::cout << "\033[" + std::to_string(m_line_number + 1) + ";1H";
 
 		for (int i = 0; i < 139; i++) {
+			if (m_highlight_begin != m_highlight_end) {
+				if (m_highlight_begin == i) {
+					std::cout << "\033[3;100;30m";// Texting\033[0m
+				}
+				if (m_highlight_end == i) {
+					std::cout << "\033[0m";
+				}
+			}
 			if (m_data[i] == 0) {
 				std::cout << ' ';
 			}
-			std::cout << m_data[i];
+			else {
+				std::cout << m_data[i];
+			}			
 		}
 	}
 
+	void DelChar(int x) {
+		for (int i = x; i < 138; i++) {
+			m_data[i] = m_data[i + 1];
+		}
+
+		Print();
+	}
 	void AddChar(char ch, int x) {
 		if (m_data[139]) return;
 
 		for (int i = 139; i >= x; i--) {
 			m_data[i] = m_data[i - 1];
 		}
-		/*if (m_data[139]) return;
 
-		for (int i = x-1; i < 139; i++) {
-			m_data[i + 1] = m_data[i];
-		}*/
 		m_data[x] = ch;
 
 		Print();
 	}
 	void InsertChar(char ch, int x) {
-		m_data[x] = ch;
+		m_data[x - 1] = ch;
 
 		Print();
+	}
+
+	void ClearHighlight() {
+		m_highlight_begin = 0;
+		m_highlight_end = 0;
+	}
+
+	void Copy() {
+		if (OpenClipboard(NULL)) {
+			EmptyClipboard(); 
+
+			std::vector<char> bb;
+			int i = m_highlight_begin;
+			while (i != m_highlight_end) {
+				bb.push_back(m_data[i]);
+
+				i++;
+			}
+			bb.push_back(0);
+
+			HGLOBAL hGlobal = GlobalAlloc(GMEM_MOVEABLE, bb.size() + 1);
+			if (hGlobal) {
+				memcpy(GlobalLock(hGlobal), bb.data(), bb.size() + 1);
+				GlobalUnlock(hGlobal);
+				SetClipboardData(CF_TEXT, hGlobal);
+			}
+			CloseClipboard();
+		}
 	}
 
 private:
 	int m_line_number;
 
 	char m_data[140]{};
+
+	///copy past
+	int m_highlight_begin = 0;
+	int m_highlight_end = 0;
 };
 
 class Document {
@@ -61,45 +110,185 @@ public:
 	}
 
 	void MoveCursorLeft() {
+		ClearHighlg();
+
 		if (m_cursor_pos_x > 0) {
 			m_cursor_pos_x--;
 		}
 		SetCarriage();
 	}
 	void MoveCursorRight() {
+		ClearHighlg();
+
 		if (m_cursor_pos_x < 139) {
 			m_cursor_pos_x++;
 		}
 		SetCarriage();
 	}
+	void MoveCursorDown() {
+		ClearHighlg();
 
+		m_cursor_pos_y++;
+
+		if(m_lines.size()-1 < m_cursor_pos_y){
+			m_lines.push_back(Line(m_cursor_pos_y));
+		}
+
+		SetCarriage();
+	}
+	void MoveCursorUp() {
+		ClearHighlg();
+
+		if (m_cursor_pos_y == 0) return;
+
+		m_cursor_pos_y--;
+
+		SetCarriage();
+	}
 	
+	void NewLine() {
+		ClearHighlg();
 
-	void AddChar(bool is_insert_mode, char ch) {
-		if (is_insert_mode) {
-			m_lines[m_cursor_pos_y].InsertChar(ch, m_cursor_pos_x);
+		m_cursor_pos_y++;
+
+		m_lines.push_back(Line(0));
+
+		for (int i = m_lines.size() - 1; i > m_cursor_pos_y; i--) {
+			m_lines[i] = m_lines[i - 1];
+		}
+
+		m_lines[m_cursor_pos_y] = Line(m_cursor_pos_y);
+
+		m_cursor_pos_x = 0;
+
+		for (int i = 0; i < m_lines.size(); i++) {
+			m_lines[i].Print();
+		}
+
+		SetCarriage();
+	}
+
+	void HighlightLeft() {
+		if (m_cursor_pos_x > 0) {
+			m_cursor_pos_x--;
+		}
+		SetCarriage();
+
+		if (m_lines[m_cursor_pos_y].m_highlight_end == 0) {
+			m_lines[m_cursor_pos_y].m_highlight_end = m_cursor_pos_x + 1;
+		}
+		m_lines[m_cursor_pos_y].m_highlight_begin = m_cursor_pos_x;
+
+		m_lines[m_cursor_pos_y].Print();
+		SetCarriage();
+	}
+	void HighlightRight() {
+		if (m_cursor_pos_x < 139) {
 			m_cursor_pos_x++;
-			SetCarriage();
-		}	
-		else {
-			if (!m_lines[m_cursor_pos_y].m_data[138]) {
-				m_lines[m_cursor_pos_y].AddChar(ch, m_cursor_pos_x);
-				m_cursor_pos_x++;
+		}
+
+		SetCarriage();
+
+		if (m_lines[m_cursor_pos_y].m_highlight_begin == 0) {
+			m_lines[m_cursor_pos_y].m_highlight_begin = m_cursor_pos_x - 1;
+		}
+		m_lines[m_cursor_pos_y].m_highlight_end = m_cursor_pos_x;
+
+		m_lines[m_cursor_pos_y].Print();
+		SetCarriage();
+	}
+
+
+	void DelChar() {
+		ClearHighlg();
+
+		if (m_is_allowed_editing) {
+			if (m_cursor_pos_x > 0) {
+				MoveCursorLeft();
+				m_lines[m_cursor_pos_y].DelChar(m_cursor_pos_x);
 				SetCarriage();
-			}		
+			}	
 		}
 	}
 
+	void AddChar(char ch) {
+		ClearHighlg();
+
+		if (m_is_allowed_editing) {
+			UpdateInformation();
+			if (m_is_insert) {
+				MoveCursorRight();
+				m_lines[m_cursor_pos_y].InsertChar(ch, m_cursor_pos_x);
+				SetCarriage();
+			}
+			else {
+				if (!m_lines[m_cursor_pos_y].m_data[138]) {
+					m_lines[m_cursor_pos_y].AddChar(ch, m_cursor_pos_x);
+					MoveCursorRight();
+				}
+			}
+		}		
+	}
+
 	void Edit();
+
+	void UpdateInformation() {
+		SetCarriage(0, 40);
+		char user[]{ "admin" };
+		auto now = std::chrono::system_clock::now();
+		std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+
+		std::tm* now_tm = std::localtime(&now_time_t);
+
+		std::cout << "\033[2K";
+		std::cout << "\033[32m" << user << (m_is_allowed_editing ? ": " : "(Read-only): ") << "\033[0m" << std::put_time(now_tm, "%Y-%m-%d %H:%M:%S") << (m_is_insert ? " (insert)" : "");//m_current_user->GetLogin();
+		SetCarriage();
+	}
+
+	void SwitchInsert() {
+		ClearHighlg();
+
+		if (!m_is_allowed_editing) return;
+
+		m_is_insert = (m_is_insert ? false : true);
+
+		UpdateInformation();
+	}
+
+	void ClearHighlg() {
+		for(int i = 0; i < m_lines.size(); i++) {
+			m_lines[i].ClearHighlight();
+			m_lines[i].Print();
+		}
+	}
+
+	void CopyToClipboard() {
+		m_lines[m_cursor_pos_y].Copy();
+	}
+	void PastFromClipboard() {
+
+	}
 
 private:
 	void SetCarriage() {
 		std::cout << "\033[" + std::to_string(m_cursor_pos_y + 1) + ";" + std::to_string(m_cursor_pos_x + 1) + "H";
 	}
+	void SetCarriage(int x, int y) {
+		std::cout << "\033[" + std::to_string(y + 1) + ";" + std::to_string(x + 1) + "H";
+	}
 
 private:
+	bool m_is_allowed_editing = true;
+
+	User* m_current_user;
+
 	int m_cursor_pos_x = 0;
 	int m_cursor_pos_y = 0;
 
 	std::vector<Line> m_lines;
+
+	std::vector<Line> m_undo_lines;
+
+
+	bool m_is_insert = false;
 };
