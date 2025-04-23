7d9cc9fefca83aca502c6028aeb922068dcecae0
Fiodar Kavalchuk <141013183+EidanowI@users.noreply.github.com>
2025-04-23 09:56:26 +0300

    Add line class

--- Lab2/console_text_editor/console_text_editor/Document/Content.h
+++ Lab2/console_text_editor/console_text_editor/Document/Content.h
@@ -2,6 +2,29 @@
 
 
 
+class Line {
+public:
+	Line(char* data) {
+		for (int i = 0; i < 140; i++) {
+			if (data[i]) {
+				m_data[i] = data[i];
+			}
+			else {
+				break;
+			}
+		}
+	}
+
+	void SetLineNumber(int line_number) {
+		m_line_number = line_number;
+	}
+
+private:
+	int m_line_number;
+
+	char m_data[140]{};
+};
+
 class Content {
 public:
 	Content() {
@@ -15,6 +38,11 @@ public:
 		return true;
 	}
 
-private:
+	void AddLine(Line line) {
+		line.SetLineNumber(m_lines.size());
+		m_lines.push_back(line);
+	}
 
+private:
+	std::vector<Line> m_lines;
 };
